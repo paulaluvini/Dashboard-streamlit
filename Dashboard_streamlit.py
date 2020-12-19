3b186cc425725838850a3081edb15f6e2bb05cc3
paulaluvini <paulaluvini@DESKTOP-AGE9Q2B.localdomain>
2020-12-19 19:04:06 -0300

    second

--- Dashboard_streamlit.py
+++ Dashboard_streamlit.py
@@ -4,6 +4,18 @@ import streamlit as st
 import pandas as pd
 import requests
 
+st.markdown(
+            """
+            <style>
+            .css-1aumxhk.e1fqkh3o1 {
+                    background-image: linear-gradient(#00B7BD,#00B7BD);
+                            color: white
+                            }
+                    </style>
+                    """,
+                        unsafe_allow_html=True,
+                        )
+
 URL_LOGIN = 'http://miweb-dev22.us-east-1.elasticbeanstalk.com/api-token-auth/'
 data = {'username':'facundoprueba','password':'Palmera01'}
 res_request_login = requests.post(URL_LOGIN, data)
@@ -28,11 +40,56 @@ emails_df['length'] = emails_df['emails'].apply(len)
 # Sidebar
 st.sidebar.title("Dashboard con Métricas de API:")
 st.sidebar.title("Métricas")
-select = st.sidebar.selectbox('Seleccione', ['Longitud de emails', 'Nube de Palabras'], key='1')
+select = st.sidebar.selectbox('Seleccione', ['Longitud de emails', 'Nube de Palabras', 'Palabras más frecuentes'],
+        key='1')
 
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
+from collections import Counter  
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize  
+
 largo = emails_df["length"]
+comment_words = ''
+stopwords = set(STOPWORDS)
+for val in emails_df['emails']:
+    val = str(val)
+    # split the value
+    tokens = val.split()
+    # Converts each token into lowercase
+    for i in range(len(tokens)):
+        tokens[i] = tokens[i].lower()
+    comment_words += " ".join(tokens)+" "
+wordcloud = WordCloud(width = 1000, height = 800,background_color ='black',colormap = "Pastel1",\
+        stopwords = stopwords, min_font_size = 20).generate(comment_words) 
+
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+# remove punctuation from the string
+no_punct = ""
+for char in comment_words:
+    if char not in punctuations:
+        no_punct = no_punct + char
+        # display the unpunctuated string
+comment_words = no_punct
+word_tokens = word_tokenize(comment_words)
+filtered_sentence = [w for w in word_tokens if not w in stopwords]
+filtered_sentence = []
+for w in word_tokens:
+    if w not in stopwords:
+        filtered_sentence.append(w)
+comment_words = filtered_sentence
+# Pass the split_it list to instance of Counter class.
+Counter = Counter(comment_words)
+most_occur = Counter.most_common(10)
+palabra = []
+cantidad= []
+for i in range(10):
+    palabra.append(most_occur[i][0])
+    cantidad.append(most_occur[i][1])
+mas_frecuentes = pd.DataFrame()
+mas_frecuentes['palabra'] = palabra
+mas_frecuentes['cantidad'] = cantidad
 
 
 st.title("Clasificación de emails en Spam y Ham")
@@ -57,24 +114,12 @@ if select == 'Longitud de emails':
                 st.set_option('deprecation.showPyplotGlobalUse', False)
                 st.pyplot()
 if select == 'Nube de Palabras':
-                comment_words = '' 
-                stopwords = set(STOPWORDS)
-                #stopwords = set(stopwords.words('english')) 
-                for val in emails_df['emails']: 
-                    # typecaste each val to string 
-                    val = str(val) 
-                    # split the value 
-                    tokens = val.split() 
-                    # Converts each token into lowercase 
-                    for i in range(len(tokens)):
-                        tokens[i] = tokens[i].lower() 
-                    comment_words += " ".join(tokens)+" "
-                wordcloud = WordCloud(width = 1000, height = 800, background_color ='black', colormap = "Pastel1",
-                        stopwords = stopwords, min_font_size = 20).generate(comment_words) 
                 plt.figure(figsize = (8, 8), facecolor = None) 
                 plt.title("Palabras más importantes", size = 24)
                 plt.imshow(wordcloud) 
                 plt.axis("off") 
                 plt.tight_layout(pad = 0) 
                 st.pyplot()
-
+if select == 'Palabras más frecuentes':
+    st.header("Las 10 palabras más frecuentes")
+    st.table(mas_frecuentes.assign(hack='').set_index('hack'))
